a1e185020dbce844833241008d71a79df269cc2a
John Cooley <john.cooley@manchester.ac.uk>
2020-05-22 17:24:05 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace MyNewRepoGitTest
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
